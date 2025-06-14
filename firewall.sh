2906ef27f37e28d81c4d7b7190a47260cf05601d
Arash Ariaye <ghost.a78@gmail.com>
2025-06-14 18:08:03 +0330

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -6,10 +6,10 @@ MY_IP="91.107.180.29"
 # File movaghat baraye zakhire IP-ha
 TMP_IP_FILE="/tmp/blocked_ips.txt"
 
-echo "[+] Dar hale jam'avari IP-ha..."
+echo "[+] IP ha dar hale jam'avari hastan..."
 IP_LIST=$(sudo tcpdump -i eth0 -nn -c 10000 | awk '{print $3}' | cut -d'.' -f1-4 | sort | uniq -c | sort -nr | head -20 | awk '{print $2}' | grep -v "$MY_IP")
 
-# Neveshtane IP-ha dar file movaghat
+# Save IP-ha dar file movaghat
 echo "$IP_LIST" > "$TMP_IP_FILE"
 
 echo "[+] Dar hale block kardan-e IP-ha..."
@@ -18,12 +18,9 @@ for IP in $IP_LIST; do
     sudo iptables -A OUTPUT -d $IP -j DROP
 done
 
-# Save kardan rules
-sudo netfilter-persistent save
+echo "[✓] IP-ha block shodan. Sabr kon 24 saat..."
 
-echo "[+] IP-ha block shodan. montazer 24 saat mimunim..."
-
-# Entezar 24 saat (86400 saniye)
+# 24 saat = 86400 saniye
 sleep 86400
 
 echo "[+] Dar hale hazf kardan-e rules..."
@@ -32,10 +29,7 @@ for IP in $(cat "$TMP_IP_FILE"); do
     sudo iptables -D OUTPUT -d $IP -j DROP
 done
 
-# Save kardan rules baraye marhale payani
-sudo netfilter-persistent save
-
-# Paak kardan file movaghat
+# Delete file movaghat
 rm -f "$TMP_IP_FILE"
 
-echo "[✓] Hame chiz pak shod!"
+echo "[✓] Hame chiz tamiz shod!"
