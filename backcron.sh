16a37fd2e379ac996417994d2af82ff0070bcc6c
Arash Ariaye <ghost.a78@gmail.com>
2025-06-17 17:14:41 +0330

    Create backcron.sh

---
+++ backcron.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script file name
+SCRIPT_PATH="/root/restart-backhauls.sh"
+
+# Create file and write the script inside
+echo '#!/bin/bash
+systemctl list-unit-files | grep "backhaul-ir" | awk "{print \$1}" | while read svc; do
+    systemctl restart "$svc"
+done' > "$SCRIPT_PATH"
+
+# Make the file executable
+chmod +x "$SCRIPT_PATH"
+
+# Add cron job
+CRON_JOB="*/14 * * * * $SCRIPT_PATH"
+(crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+
+echo "Script va cron job ba movafaghiat set shod."
